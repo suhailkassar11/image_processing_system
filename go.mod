77bb5e6fcb4145e9d953413058b5a7c3af1ac852
suhailkassar11 <suhail.1923co1011@kiet.edu>
2025-05-19 23:08:28 +0530

    setup project

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/suhailkassar11/image_processing_system
+
+go 1.23.4
+
+require gorm.io/gorm v1.26.1
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
---
+++ models/user.go
@@ -0,0 +1,15 @@
+package user
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" gorm:"not null" validate:"required"`
+	Email    string `json:"email" gorm:"not null" validate:"required"`
+	Password string `json:"password" gorm:"not null" validate:"required"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" gorm:"not null" validate:"required"`
+	Password string `json:"email" gorm:"not null" validate:"required"`
+}
